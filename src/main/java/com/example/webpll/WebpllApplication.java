ddc9fa3faa31d6100c817d8acfbc748f1801f2e7
Irishamy <irishamy@student.21-school.ru>
2023-08-30 17:24:10 +0300

    first simple app

---
+++ src/main/java/com/example/webpll/WebpllApplication.java
@@ -0,0 +1,13 @@
+package com.example.webpll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webpll/WebpllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webpll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
